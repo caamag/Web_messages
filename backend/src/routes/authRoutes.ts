47e8fe14fb328518061c8f44c92d444dcc29c198
Cainho <caiolopesfv@gmail.com>
2025-02-25 21:19:14 -0300

    Removendo backend

--- backend/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { register, login } from "../controllers/authController";
-
-const router = express.Router();
-
-router.post("/auth/register", register);
-router.post("/auth/login", login);
-
-export default router;
